8f803285e32ccf29132f20d66fec86035da901c9
lzeman <carlosluciano.garcia@students.salle.url.edu>
2020-03-07 18:36:15 +0100

    Extracting Buzz

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -3,12 +3,19 @@ package fizz_buzz_kata
 func FizzBuzz(number int) string {
 	if isFizzBuzz(number) {
 		return "FizzBuzz"
-	} else if number%5 == 0 {
+	} else if isBuzz(number) {
 		return "Buzz"
 	} else if number%3 == 0 {
 		return "Fizz"
 	} else {
 		return ""
+	}
+}
+func isBuzz(number int) bool {
+	if number%5 == 0 {
+		return true
+	} else {
+		return false
 	}
 }
 func isFizzBuzz(number int) bool {
