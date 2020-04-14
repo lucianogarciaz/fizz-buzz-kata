eb6b09ec615db58e908bc66120ee5e95ebafba7e
lzeman <carlosluciano.garcia@salle.url.edu>
2020-04-14 12:29:49 +0200

    testing fizzbuzz output names

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -1,50 +1,51 @@
-package fizz_buzz_kata
+package main
 
 import "testing"
 
 type dataTest struct {
 	input    int
-	expected string
+	expected interface{}
 }
 
-func Test_Data(t *testing.T) {
+func Test_FizzBuzzNames(t *testing.T) {
 	for _, testData := range setUpData() {
-		if FizzBuzz(testData.input) != testData.expected {
-			t.Error("The expected value was: ", testData.expected, " instead ", testData.input)
+		object := Factory(testData.input)
+		if object != nil && object.GetName() != testData.expected {
+			t.Error("The expected value was: ", testData.expected, " instead ", object.GetName())
 		}
 	}
 }
 func setUpData() []dataTest {
 	return []dataTest{
-		{1, ""},
-		{2, ""},
+		{1, 1},
+		{2, 2},
 		{3, "Fizz"},
-		{4, ""},
+		{4, 4},
 		{5, "Buzz"},
 		{6, "Fizz"},
-		{7, ""},
-		{8, ""},
+		{7, 7},
+		{8, 8},
 		{9, "Fizz"},
 		{10, "Buzz"},
-		{11, ""},
+		{11, 11},
 		{12, "Fizz"},
-		{13, ""},
-		{14, ""},
+		{13, 13},
+		{14, 14},
 		{15, "FizzBuzz"},
-		{16, ""},
-		{17, ""},
+		{16, 16},
+		{17, 17},
 		{18, "Fizz"},
-		{19, ""},
+		{19, 19},
 		{20, "Buzz"},
 		{21, "Fizz"},
-		{22, ""},
-		{23, ""},
+		{22, 22},
+		{23, 23},
 		{24, "Fizz"},
 		{25, "Buzz"},
-		{26, ""},
+		{26, 26},
 		{27, "Fizz"},
-		{28, ""},
-		{29, ""},
+		{28, 28},
+		{29, 29},
 		{30, "FizzBuzz"},
 	}
 }
